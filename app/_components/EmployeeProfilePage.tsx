98f320b4696c578f6be8bca37cf911a6980e9174
Bradly Chauke <bradlychauke@gmail.com>
2026-03-12 07:41:48 +0200

    aligned EmployeeProfilePage layout with back button at start

--- app/_components/EmployeeProfilePage.tsx
+++ app/_components/EmployeeProfilePage.tsx
@@ -8,7 +8,7 @@ import ProfileTabs from "@/app/_components/ProfileTabs";
 import { getSessionAction } from "@/lib/actions/sessionActions";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { RiArrowGoBackFill } from "react-icons/ri";
+import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { EmployeeProfilePageProps } from "@/types";
 
 const EmployeeProfilePage = async ({
@@ -31,18 +31,19 @@ const EmployeeProfilePage = async ({
 
   return (
     <div className="px-4">
-      <div className="flex flex-col mt-10 items-center gap-6">
+      <div className="flex  max-w-2xl mx-auto flex-col gap-6 mt-10 ">
         {backHref && (
-          <div className="flex items-center">
-            <Button className="w-fit mt-20 bg-gray-100 hover:translate-y-1 hover:bg-gray-200 shadow text-gray-500 text-sm flex items-center  hover:text-gray-600 hover:cursor-pointer">
-              <Link href={backHref}>
-                <RiArrowGoBackFill />
-              </Link>
-            </Button>
-          </div>
+          <Button className="w-fit mt-20 bg-gray-100 hover:translate-y-1 hover:bg-gray-200 shadow text-gray-500 text-sm flex items-center  hover:text-gray-600 hover:cursor-pointer">
+            <Link href={backHref}>
+              <div className="flex items-center gap-2">
+                <MdOutlineKeyboardBackspace />
+                Back to Table
+              </div>
+            </Link>
+          </Button>
         )}
 
-        <div className="mt-20 flex justify-between gap-3 w-full max-w-2xl">
+        <div className="mt-2 flex justify-between gap-3 w-full max-w-2xl">
           <div className="shrink-0">
             <EmployeeForm employee={employed} role={role} />
           </div>
@@ -51,7 +52,10 @@ const EmployeeProfilePage = async ({
               employeeId={id}
               role={role}
               sessionId={sessionId}
-              employees={allEmployees.map((e) => ({ id: e.id!, name: e.name }))}
+              employees={allEmployees.map((e) => ({
+                id: e.id!,
+                name: e.name,
+              }))}
               shifts={shifts}
               totalMinutes={totalMinutes}
               isOwnProfile={isOwnProfile}
